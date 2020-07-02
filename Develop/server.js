b29b7482b7607d4b5c10fbabd681b2a8a50a2065
mjaeschke <jaeschke21@gmail.com>
2020-07-02 17:02:20 -0500

    Update server.js
    for some reason the delete wasnt working so i deleted and re pasted it now it works also added a better log for when you delete something

--- Develop/server.js
+++ Develop/server.js
@@ -54,15 +54,23 @@ app.post("/api/notes", function(req, res) {
   console.log("Note saved to db.json. Content: ", newNote);
   res.json(savedNotes);
 })
-  app.delete("api/notes/:id",function(rew, res){
-    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    let noteID = req.params.id;
-    let newID = 0;
-    console.log(`Deleting note`);
-    savedNotes = savedNotes.filter(currNote => {
+app.delete("/api/notes/:id", function(req, res) {
+  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let noteID = req.params.id;
+  let newID = 0;
+  console.log(`Deleting note with ID ${noteID}`);
+  savedNotes = savedNotes.filter(currNote => {
       return currNote.id != noteID;
   })
   
+  for (currNote of savedNotes) {
+      currNote.id = newID.toString();
+      newID++;
+  }
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+  res.json(savedNotes);
+})
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
